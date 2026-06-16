9a5f4ea7cbecc23e8b623b998c7d47fc6084f817
aloelose <aloelose@mail.ru>
2026-06-17 02:28:29 +0500

    Добавлен favicon

--- client/src/boot/axios.js
+++ client/src/boot/axios.js
@@ -1,7 +1,7 @@
 import { boot } from 'quasar/wrappers';
 import axios from 'axios';
 
-const api = axios.create({ baseURL: 'http://127.0.0.1:8000/api' });
+const api = axios.create({ baseURL: 'https://api.profmastergrad.ru/api' });
 
 export default boot(({ app }) => {
   app.config.globalProperties.$axios = axios;
